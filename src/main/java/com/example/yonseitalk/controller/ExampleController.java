298ad1e95baa7121b0e2c7783caf875f1dd85a87
Hoyong Jung <hoyong.jung.dev@gmail.com>
2021-10-12 11:04:07 +0900

    add example controller

---
+++ src/main/java/com/example/yonseitalk/controller/ExampleController.java
@@ -0,0 +1,15 @@
+package com.example.yonseitalk.controller;
+
+import com.example.yonseitalk.view.ExampleView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("example")
+public class ExampleController {
+    @GetMapping("")
+    public ExampleView example() {
+        return new ExampleView("Sample Message");
+    }
+}
---
+++ src/main/java/com/example/yonseitalk/view/ExampleView.java
@@ -0,0 +1,13 @@
+package com.example.yonseitalk.view;
+
+public class ExampleView {
+    private String exampleProperty;
+
+    public ExampleView(String exampleProperty) {
+        this.exampleProperty = exampleProperty;
+    }
+
+    public String getExampleProperty() {
+        return exampleProperty;
+    }
+}
