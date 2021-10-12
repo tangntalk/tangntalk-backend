c59559b475e5ce33b9b0c6b81db04238c5514f92
Hoyong Jung <hoyong.jung.dev@gmail.com>
2021-10-12 10:19:12 +0900

    init server code

---
+++ src/main/java/com/example/yonseitalk/YonseitalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.yonseitalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonseitalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonseitalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yonseitalk/YonseitalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yonseitalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonseitalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
