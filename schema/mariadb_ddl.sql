f95aa78e5c3b651d89afab81321a87bb11536fba
flaxinger <70012548+flaxinger@users.noreply.github.com>
2021-10-28 01:38:59 +0900

    DB-4   - Message Table 변경     - Rendezvous flag, Rendezvous time, read time chatroom ID 추가   - 속성 리뷰     - Auto Increment 추가     - 공간 랑데뷰 기본값 insert, Default값 저장

--- schema/mariadb_ddl.sql
+++ schema/mariadb_ddl.sql
@@ -11,16 +11,23 @@ CREATE TABLE IF NOT EXISTS location(
 ALTER TABLE location
     ADD CONSTRAINT location_pk PRIMARY KEY (name);
 
+INSERT IGNORE INTO location VALUES ('공학관');
+INSERT IGNORE INTO location VALUES ('백양관');
+INSERT IGNORE INTO location VALUES ('학생회관');
+INSERT IGNORE INTO location VALUES ('언더우드관');
+
+ALTER TABLE location
+    ALTER name SET DEFAULT '공학관';
 
 # yt_user table -------------------------------------
 CREATE TABLE IF NOT EXISTS yt_user(
-    user_id varchar(255) NOT NULL UNIQUE,
+    user_id varchar(255) NOT NULL UNIQUE AUTO_INCREMENT,
     name varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
     user_time timestamp NOT NULL,
     status_message varchar(255) NOT NULL,
     type varchar(255) NOT NULL,
-    connection_status boolean NOT NULL,
+    #connection_status boolean NOT NULL,
     user_location varchar(255) NOT NULL
 );
 
@@ -38,35 +45,14 @@ CREATE TABLE IF NOT EXISTS friends(
 );
 
 ALTER TABLE friends
-    ADD CONSTRAINT friends_pk foreign key (user_id) REFERENCES yt_user(user_id);
+    ADD CONSTRAINT friends_pk FOREIGN KEY (user_id) REFERENCES yt_user(user_id);
 
 ALTER TABLE friends
     ADD CONSTRAINT friends_fk FOREIGN KEY (friend_id) REFERENCES yt_user(user_id);
 
-
-# message table ------------------------------------
-CREATE TABLE IF NOT EXISTS message(
-    message_id bigint NOT NULL UNIQUE,
-    sender_id varchar(255) NOT NULL,
-    send_time timestamp NOT NULL,
-    content varchar(255) NOT NULL,
-    message_location varchar(255) NOT NULL,
-    receiver_id varchar(255) NOT NULL
-);
-
-ALTER TABLE message
-    ADD CONSTRAINT message_pk PRIMARY KEY (message_id);
-
-ALTER TABLE message
-    ADD CONSTRAINT message_location_fk FOREIGN KEY (message_location) REFERENCES location(name);
-
-ALTER TABLE message
-    ADD CONSTRAINT receiver_id_fk FOREIGN KEY (receiver_id) REFERENCES yt_user(user_id);
-
-
 # chatroom table ------------------------------------
 CREATE TABLE IF NOT EXISTS chatroom(
-    chatroom_id bigint NOT NULL UNIQUE,
+    chatroom_id bigint NOT NULL UNIQUE AUTO_INCREMENT,
     user_1 varchar(255) NOT NULL,
     user_2 varchar(255) NOT NULL,
     talk bigint NOT NULL,
@@ -80,11 +66,37 @@ ALTER TABLE chatroom
 ALTER TABLE chatroom
     ADD CONSTRAINT user_2_fk FOREIGN KEY (user_2) REFERENCES yt_user(user_id);
 
-# 회의 이후 작성할 예정
-# ALTER TABLE chatroom
-#     ADD CONSTRAINT talk_fk FOREIGN KEY (talk) REFERENCES ;
-
 ALTER TABLE chatroom
     ADD CONSTRAINT last_send_user_fk FOREIGN KEY (last_send_user) REFERENCES yt_user(user_id);
 
+
+
+# message table ------------------------------------
+CREATE TABLE IF NOT EXISTS message(
+    message_id bigint NOT NULL AUTO_INCREMENT UNIQUE,
+    chatroom_id bigint NOT NULL,
+    sender_id varchar(255) NOT NULL,
+    send_time timestamp NOT NULL,
+    content varchar(255) NOT NULL,
+    message_location varchar(255) NOT NULL,
+    receiver_id varchar(255) NOT NULL,
+    rendezvous_flag boolean,
+    rendezvous_time timestamp,
+    read_time timestamp
+);
+
+ALTER TABLE message
+    ADD CONSTRAINT message_pk PRIMARY KEY (message_id);
+
+ALTER TABLE message
+    ADD CONSTRAINT message_location_fk FOREIGN KEY (message_location) REFERENCES location(name);
+
+ALTER TABLE message
+    ADD CONSTRAINT sender_id_fk FOREIGN KEY (sender_id) REFERENCES yt_user(user_id);
+
+ALTER TABLE message
+    ADD CONSTRAINT receiver_id_fk FOREIGN KEY (receiver_id) REFERENCES yt_user(user_id);
+
+ALTER TABLE message
+    ADD CONSTRAINT chatroom_id_fk FOREIGN KEY (chatroom_id) REFERENCES chatroom(chatroom_id);
 
