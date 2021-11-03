d292b1d2d1d4f5c8efc7830adb798557a097220f
flaxinger <70012548+flaxinger@users.noreply.github.com>
2021-11-03 21:35:01 +0900

    DB-10   - UserInfoController 추가     - /user/{user_id}     - /user/{user_id}/location     - /user/{user_id}/status   - UserInfoController 테스트 작성   - User Repository의 updateUserName 삭제

---
+++ src/main/java/com/example/yonseitalk/controller/UserInfoController.java
@@ -0,0 +1,71 @@
+package com.example.yonseitalk.controller;
+
+import com.example.yonseitalk.domain.User;
+import com.example.yonseitalk.exception.NotFoundException;
+import com.example.yonseitalk.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RequestMapping("/users")
+@RestController
+public class UserInfoController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserInfoController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping(value = "/{user_id}", method = RequestMethod.GET)
+    public ResponseEntity<?> userInfo(@PathVariable("user_id") String userId){
+
+        Optional<User> user = userRepository.findById(userId);
+        Map<String, String> userInfo = new HashMap<>();
+        Map<String, Object> response = new HashMap<>();
+
+        if(!user.isPresent()) {
+            response.put("success", "false");
+            response.put("code", new NotFoundException());
+        } else{
+            response.put("success", "true");
+            userInfo.put("name", user.get().getName());
+            userInfo.put("status_message", user.get().getStatus_message());
+            userInfo.put("location_name", user.get().getUser_location());
+            response.put("user", userInfo);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/{user_id}/location")
+    public ResponseEntity<?> updateLocation(@PathVariable("user_id") String userId, @RequestBody Map<String, String> location){
+
+        int status = userRepository.updateUserLocation(userId, location.get("location_name"));
+
+        Map<String, String> response = new HashMap<>();
+        if(status > 0)
+            response.put("success", "true");
+        else
+            response.put("success", "false");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/{user_id}/status")
+    public ResponseEntity<?> updateMessage(@PathVariable("user_id") String userId, @RequestBody Map<String, String> statusMessage){
+
+        int status = userRepository.updateStatusMessage(userId, statusMessage.get("status_message"));
+
+        Map<String, String> response = new HashMap<>();
+        if(status > 0)
+            response.put("success", "true");
+        else
+            response.put("success", "false");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
--- src/test/java/com/example/yonseitalk/YonseitalkApplicationTests.java
+++ src/test/java/com/example/yonseitalk/YonseitalkApplicationTests.java
@@ -1,13 +1,119 @@
 package com.example.yonseitalk;
 
+import com.example.yonseitalk.domain.User;
+import com.example.yonseitalk.repository.DBUserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
 class YonseitalkApplicationTests {
+
+	@Autowired
+	private DBUserRepository dbUserRepository;
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void userInfo() throws Exception {
+		User user1= new User();
+		user1.setUser_id("flaxinger1");
+		user1.setName("yohan");
+		user1.setPassword("mok");
+
+		user1.setStatus_message("Preswot");
+		user1.setType("1");
+		user1.setUser_location("공학관");
+		user1.setConnection_status(true);
+
+		dbUserRepository.save(user1);
+
+		mvc.perform(get("/users/flaxinger1"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.user.name", is("yohan")))
+				.andExpect(jsonPath("$.user.status_message", is("Preswot")))
+				.andExpect(jsonPath("$.user.location_name", is("공학관")));
+	}
+
+	@Test
+	void updateMessage() throws Exception {
+		User user1= new User();
+		user1.setUser_id("flaxinger1");
+		user1.setName("yohan");
+		user1.setPassword("mok");
+
+		user1.setStatus_message("Preswot");
+		user1.setType("1");
+		user1.setUser_location("공학관");
+		user1.setConnection_status(true);
+
+		dbUserRepository.save(user1);
+
+		Map<String, String> status = new HashMap<>();
+		status.put("status_message", "아! 프레스왓!");
+
+		mvc.perform(post("/users/flaxinger1/status")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(status)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.success", is("true")));
+
+		mvc.perform(get("/users/flaxinger1"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.user.status_message", is("아! 프레스왓!")));
+	}
+
+	@Test
+	void updateLocation() throws Exception {
+		User user1= new User();
+		user1.setUser_id("flaxinger1");
+		user1.setName("yohan");
+		user1.setPassword("mok");
+
+		user1.setStatus_message("Preswot");
+		user1.setType("1");
+		user1.setUser_location("공학관");
+		user1.setConnection_status(true);
+
+		dbUserRepository.save(user1);
+
+		Map<String, String> location = new HashMap<>();
+		location.put("location_name", "백양관");
+
+		mvc.perform(post("/users/flaxinger1/location")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(location)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.success", is("true")));
+
+		mvc.perform(get("/users/flaxinger1"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.user.location_name", is("백양관")));
 	}
 
 }
