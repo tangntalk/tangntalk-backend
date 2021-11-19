f9afba65f08dbf3b58bba7f47ce645bd5504c5cf
namsick96 <namsick96@naver.com>
2021-11-19 21:49:41 +0900

    [feat] Repository Layer에 AES128 클래스 추 해당 클래스를 통해 repository layer에서 암호화 작업 수행 예정

---
+++ src/main/java/com/example/yonseitalk/repository/AES128.java
@@ -0,0 +1,15 @@
+package com.example.yonseitalk.repository;
+
+import java.security.spec.AlgorithmParameterSpec;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+
+public class AES128 {
+}
