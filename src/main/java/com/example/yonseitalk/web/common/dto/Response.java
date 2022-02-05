9f96c158b6274c1dc8359859cc134241c7bd0a65
Jaeyeon Park <donionb@yonsei.ac.kr>
2022-02-05 15:52:24 +0900

    feat : add common response dto

---
+++ src/main/java/com/example/yonseitalk/web/common/dto/Response.java
@@ -0,0 +1,61 @@
+package com.example.yonseitalk.web.common.dto;
+
+import lombok.Getter;
+
+import java.util.List;
+
+public class Response {
+
+    private static final String OK_MESSAGE = "정상적으로 처리되었습니다.";
+
+    @Getter
+    public static class Empty {
+        private final boolean success;
+        private final String message;
+
+        public Empty() {
+            success = true;
+            message = OK_MESSAGE;
+        }
+    }
+
+    @Getter
+    public static class Item<T> {
+        private final boolean success;
+        private final String message;
+        private final T result;
+
+        public Item(T result) {
+            success = true;
+            message = OK_MESSAGE;
+            this.result = result;
+        }
+    }
+
+    @Getter
+    public static class ItemList<T> {
+        private final boolean success;
+        private final String message;
+        private final List<T> result;
+        private final int totalElements;
+
+        public ItemList(List<T> result) {
+            success = true;
+            message = OK_MESSAGE;
+            this.result = result;
+            totalElements = result.size();
+        }
+    }
+
+    @Getter
+    public static class Error {
+        private final boolean success;
+        private final String message;
+
+        public Error(String message) {
+            success = false;
+            this.message = message;
+        }
+    }
+
+}
