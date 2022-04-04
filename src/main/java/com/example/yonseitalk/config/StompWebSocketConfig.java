40b5cea8082de5a91d6333b163fd424d26c3dae6
Jaeyeon Park <donionb@yonsei.ac.kr>
2022-04-04 21:28:39 +0900

    feat : add StompWebSocket Config Class

---
+++ src/main/java/com/example/yonseitalk/config/StompWebSocketConfig.java
@@ -0,0 +1,39 @@
+package com.example.yonseitalk.config;
+
+import com.example.yonseitalk.controller.StompHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@EnableWebSocketMessageBroker
+@Configuration
+@RequiredArgsConstructor
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*");
+    }
+
+    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
+
+}
